b12e3ddc9776de2c9b29b1dd405800afb2f6b763
Kingjifront <pradhyumsingh91@example.com>
2024-02-14 14:11:35 +0530

    dags

--- app.py
+++ app.py
@@ -6,12 +6,26 @@ from src.my_mlproject.components.data_transformation import DataTransformationCo
 from src.my_mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
 
 import sys
+import mlflow
 
 
 if __name__=="__main__":
     logging.info("The execution has started")
 
     try:
+        # Add debugging statements here
+        mlflow_tracking_uri = "https://dagshub.com/Pradhyumn1/sudent_data_insights.mlflo"
+        mlflow_tracking_username = "Pradhyumn1"
+        mlflow_tracking_password = "9668024cdf3cab6718afb606d69ebde8f71b7dcf"
+
+        print("MLflow Tracking URI:", mlflow_tracking_uri)
+        print("MLflow Tracking Username:", mlflow_tracking_username)
+        print("MLflow Tracking Password:", mlflow_tracking_password)
+
+        mlflow.set_registry_uri(mlflow_tracking_uri)
+        mlflow.set_tracking_uri(mlflow_tracking_uri)
+
+        
         #data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
